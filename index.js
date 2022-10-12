f242f480fddc1acfe8382c966ef7b51f775a8f53
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-10-12 17:03:16 -0500

    changes to inquirer prompts

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Packages required 
+const { get } = require('http');
 const inquirer = require('inquirer');
-const fs = require('fs').promises
+const fs = require('fs').promises;
 
 // Constant with parameters to input information from the user answers into the template literal
-const generateHTML = ({names, role, id, email, number, github, school}) =>
+const generateHTML = ({manName, engName, intName, manId, engId, intId, manEmail, engEmail, intEmail, number, github, school}) =>
 
 //Template literal for HTML to be created 
 `<!DOCTYPE html>
@@ -18,74 +19,124 @@ const generateHTML = ({names, role, id, email, number, github, school}) =>
     <header>My Team</header>
     
 <main>
-    <h2>${names}</h2>
-    <h2>${role}</h2>
-    <h4>Employee ID: ${id} </h4>
-    <h4>Email address: ${email}</h4>
+    <h2>${manName}</h2>
+    <h2>Manager</h2>
+    <h4>Employee ID: ${manId} </h4>
+    <h4>Email address: ${manEmail}</h4>
     <h4>Office Number: ${number}</h4>
 
-    <h2>${names}}</h2>
-    <h2>${role}</h2>
-    <h4>Employee ID: ${id} </h4>
-    <h4>Email address: ${email}</h4>
-    <h4>Office Number: ${github}</h4>
+    <h2>${engName}</h2>
+    <h2>Engineer</h2>
+    <h4>Employee ID: ${engId} </h4>
+    <h4>Email address: ${engEmail}</h4>
+    <h4>Githublink: ${github}</h4>
 
-    <h2>${names}}</h2>
-    <h2>${role}</h2>
-    <h4>Employee ID: ${id} </h4>
-    <h4>Email address: ${email}</h4>
-    <h4>Office Number: ${school}</h4>
+    <h2>${intName}</h2>
+    <h2>Intern</h2>
+    <h4>Employee ID: ${intId} </h4>
+    <h4>Email address: ${intEmail}</h4>
+    <h4>School: ${school}</h4>
 </main>
 </body>
 </html>`;
 
-//Employee parent class
-class Employee {
-    constructor (names, id, email){
-        this.names = names;
-        this.id = id;
-        this.email = email;
-    }
+inquirer.prompt([
+    {
+        type:'list',
+        prefix: 1,
+        name: 'role',
+        message: 'Select your new teammates role: ',
+        choices: ['Manager', 'Engineer', 'Intern']
+    },
+    {
+        type: 'input',
+        prefix: 2,
+        name: 'manName',
+        message: 'Enter your new manager name:',
+        when: (answers) => answers.role === 'Manager'
+    },
+    {
+        type: 'input',
+        prefix: 2,
+        name: 'engName',
+        message: 'Enter your new engineer name:',
+        when: (answers) => answers.role === 'Engineer'
+    },
+    {
+        type: 'input',
+        prefix: 2,
+        name: 'intName',
+        message: 'Enter you new intern name:',
+        when: (answers) => answers.role === 'Intern'
+    },
+    {
+        type: 'input',
+        prefix: 3,
+        name: 'manId',
+        message: 'Enter your new manager id: ',
+        when: (answers) => answers.role === 'Manager'
+    },
+    {
+        type: 'input',
+        prefix: 3,
+        name: 'engId',
+        message: 'Enter your new engineer id: ',
+        when: (answers) => answers.role === 'Engineer'
+    },
+    {
+        type: 'input',
+        prefix: 3,
+        name: 'intId',
+        message: 'Enter your new intern id: ',
+        when: (answers) => answers.role === 'Intern'
+    },
+    {
+        type: 'input',
+        prefix: 4,
+        name: 'manEmail',
+        message: 'Enter your new manager email: ',
+        when: (answers) => answers.role === 'Manager'
+    },
+    {
+        type: 'input',
+        prefix: 4,
+        name: 'engEmail',
+        message: 'Enter your new engineer email: ',
+        when: (answers) => answers.role === 'Engineer'
+    },
+    {
+        type: 'input',
+        prefix: 4,
+        name: 'intEmail',
+        message: 'Enter your new intern email: ',
+        when: (answers) => answers.role === 'Intern'
+    },
+    {
+        type:'input',
+        prefix: 5,
+        name: 'number',
+        message: 'Enter your new manager office number: ',
+        when: (answers) => answers.role === 'Manager'
+    },
+    {
+        type:'input',
+        prefix: 5,
+        name: 'github',
+        message: 'Enter your new engineer GitHub link: ',
+        when: (answers) => answers.role === 'Engineer'
+    },
+    {
+        type:'input',
+        prefix: 5,
+        name: 'school',
+        message: 'Enter your new interns school: ',
+        when: (answers) => answers.role === 'Intern'
+    },
+])
 
-    getName(){
-        inquirer.prompt([
-            {
-                type: 'input',
-                prefix: 1,
-                name: 'names',
-                description: 'Enter your teammates name:',
-            }
-        ])
-    };
-
-    getID(){
-        inquirer.prompt([
-            {
-                type: 'input',
-                prefix: 2,
-                name: 'id',
-                description: 'Enter your teammates id: ',
-            }
-        ])
-    };
-
-    getEmail(){
-        inquirer.prompt([
-            {
-                type: 'input',
-                prefix: 3,
-                name: 'email',
-                description: 'Enter your teammates email: ',
-            }
-        ])
-    }
-
-    getRole(){
-        console.log('Employee')
-    }
-};
-
-class Manager extends Employee{
-    constructor ()
-
-}
+.then((answers) => {
+    const htmlContent = generateHTML(answers);
+    fs.writeFile('index.html', htmlContent, (err) =>
+    err ? console.log(err) : console.log('Successfully created your team website!')
+    );
+});
